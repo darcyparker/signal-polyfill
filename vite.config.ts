5c4d56ed2eaae3f1f87765d28ba7d92b9de7e379
Johnson Chu <johnsoncodehk@gmail.com>
2025-05-01 18:25:46 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,15 +7,6 @@ const entry = join(dirname(fileURLToPath(import.meta.url)), './src/index.ts');
 
 export default defineConfig({
   plugins: [dts()],
-  test: {
-    pool: 'forks',
-    poolOptions: {
-      forks: {
-        singleFork: true,
-        execArgv: ['--expose-gc'],
-      },
-    },
-  },
   build: {
     minify: false,
     lib: {
