63e2a69ef3dec61d5aa7f2af4be5a84e80c630aa
Johnson Chu <johnsoncodehk@gmail.com>
2025-08-12 17:42:25 +0800

    Remove isAlien

--- tests/behaviors/prohibited-contexts.test.ts
+++ tests/behaviors/prohibited-contexts.test.ts
@@ -1,8 +1,6 @@
 import {describe, expect, it} from 'vitest';
 import {Signal} from '../../src/alien.js';
 
-const isAlien = true;
-
 describe('Prohibited contexts', () => {
   it('allows writes during computed', () => {
     const s = new Signal.State(1);
@@ -10,15 +8,8 @@ describe('Prohibited contexts', () => {
     expect(c.get()).toBe(2);
     expect(s.get()).toBe(2);
 
-    if (isAlien) {
-      expect(c.get()).toBe(3);
-      expect(s.get()).toBe(3);
-    } else {
-      // Note: c is marked clean in this case, even though re-evaluating it
-      // would cause it to change value (due to the set inside of it).
-      expect(c.get()).toBe(2);
-      expect(s.get()).toBe(2);
-    }
+    expect(c.get()).toBe(3);
+    expect(s.get()).toBe(3);
 
     s.set(3);
 
